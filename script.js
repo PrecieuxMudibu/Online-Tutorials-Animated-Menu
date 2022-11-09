08131ad911480ed282cd210a563f87116dd8887e
PrecieuxMudibu <oceangpmb@gmail.com>
2022-11-09 14:12:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// let navigation = document.querySelector('.navigation');
+// document.querySelector('.toggle').addEventListener('click', function() {
+//     alert('hello')
+//     this.classList.toggle('active');
+//     navigation.classList.toggle('active');
+// }) 
